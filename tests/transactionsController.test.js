2e65fe5ff458516ab4337c5765c2fdc38fb7fed2
Tyrell <tyrellkamara-oliver@pursuit.org>
2023-10-27 13:31:48 -0400

    installing and adding cors

--- tests/transactionsController.test.js
+++ tests/transactionsController.test.js
@@ -1,12 +1,29 @@
 const request = require("supertest");
 const transactionsData = require("../models/transactionsData");
-const app = require("../app");
+const transactions = require("../app");
 
-describe("GET /transactions", () => {
+describe("Index Route GET /transactions", () => {
   it("should return the transactions array", async () => {
-    const response = await request(app).get("/transactions");
+    const response = await request(transactions).get("/transactions");
 
     expect(response.status).toBe(200);
     expect(response.body).toEqual(transactionsData);
+  });
+});
+
+describe("Show Route GET /transactions/:id", () => {
+  it("should return a specific index in the array", async () => {
+    //   const {id}=res.params
+    const response = await request(transactions).get("/transactions/0");
+
+
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual(transactionsData[0]);
+  });
+  it("sends an error when an invalid index is given", async () => {
+    const response = await request(transactions).get("/transactions/9001");
+
+    expect(response.status).toBe(404)
+    expect(response.body).toBe()
   });
 });
