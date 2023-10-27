89472936480adf668472e4b55f975924081c85b6
Tyrell <tyrellkamara-oliver@pursuit.org>
2023-10-27 11:40:52 -0400

    created the routes

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 
 const transactions = require("./controllers/transactionsController");
 
+app.use(express.json());
+
 app.use("/transactions", transactions);
 
 app.get("/", (req, res) => {
--- controllers/transactionsController.js
+++ controllers/transactionsController.js
@@ -1,10 +1,48 @@
-const express = require('express')
-const transactions = express.Router()
-const transactionsData = require('../models/transactionsData');
+const express = require("express");
+const transactions = express.Router();
+const transactionsData = require("../models/transactionsData");
 
-transactions.get('/',(req,res)=>{
-    res.send('<h1>The Transactions Route</h1>')
-})
+// Index Route
+transactions.get("/", (req, res) => {
+  res.status(200).json(transactionsData);
+});
 
+// Create Route
+transactions.post("/", (req, res) => {
+  transactionsData.push(req.body);
+  res.status(200).json(transactionsData);
+});
 
-module.exports= transactions;
+// Show Route
+transactions.get("/:id", (req, res) => {
+  const { id } = req.params;
+  if (transactionsData[id]) {
+    res.status(200).json(transactionsData[id]);
+  } else {
+    res.status(404).send({ error: "Page Not Found" });
+  }
+});
+
+// Update Route
+transactions.put("/:id", (req, res) => {
+  const { id } = req.params;
+  transactionsData[id] = req.body;
+  if (transactionsData[id]) {
+    res.status(200).json(transactionsData);
+  } else {
+    res.status(404).send({ error: "Page Not Found" });
+  }
+});
+
+// Delete Route
+transactions.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  if (transactionsData[id]) {
+    transactionsData.splice(id, 1);
+    res.json(transactionsData);
+  } else {
+    res.status(404).send({ error: "Page Not Found" });
+  }
+});
+
+module.exports = transactions;
